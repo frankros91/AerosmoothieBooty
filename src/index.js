54a9d7f34e10b9f7980424528808f355d3e78927
Frank Rossi <FrankRossi@C02D2B74MD6M.fios-router.home>
2021-06-24 23:41:41 -0400

    gateway

---
+++ src/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var cors = require('cors')
+var axios = require('axios');
+var app = express();
+app.use(cors())
+const API_KEY = 'NHzsKq6Ci7LFe8lIDxNqtzMgQjQzeLLriWLx89LWhYEFDIYkOPNKAqpPqL9ets6D';
+
+app.listen(3030, () => {
+    console.log('Server started and listening on port 3000')
+
+    app.get('/', async (req, res) => {
+        const result = await axios.get(req.query.url, {
+            headers: { 'Authorization': 'Bearer ' + API_KEY }
+        })
+        res.status(200).send(result.data.response)
+    })
+})
